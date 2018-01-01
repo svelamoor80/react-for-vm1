f027ed94f3b923e48ea73125366adb2b68e1733c
Sangeetha Velamoor <svelamoor@vestmark.com>
2018-01-01 11:11:31 -0500

    Fixed some linting issues

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+//import App from './App';
 import AccountWorksheetToolbar from './AccountWorksheet';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-var element = React.createElement('h1', {className: 'greeting'}, 'Hello World!');
 //ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<AccountWorksheetToolbar />, document.getElementById('root'));
 registerServiceWorker();
